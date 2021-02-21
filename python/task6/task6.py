38666d2a30d8c1a6ab96bc08b35960df1811fa66
siddhantwho <71570870+siddhantwho@users.noreply.github.com>
2021-02-21 12:18:00 -0500

    made changes to task6.py

--- python/task6/task6.py
+++ python/task6/task6.py
@@ -2,6 +2,31 @@
 1. Write a program in Python to find out the character in a string which is uppercase using list
 comprehension.
 """
-def upper(some_list):
-    return ([x for x in some_list if x.isupper()])
+def upper(some_string):
+    return ([x for x in some_string if x.isupper()])
 
+"""
+2. Write a program to construct a dictionary from the two lists containing the names of students and
+their corresponding subjects. The dictionary should map the students with their respective subjects.
+Let’s see how to do this using for loops and dictionary comprehension.
+"""
+def makeDict(somekeys,somevals):
+    items = list(zip(somekeys,somevals))
+    
+    return {k:v for (k,v) in items}
+
+"""
+3. Learn More about Yield, next and Generators
+"""
+
+"""
+4. Write a program in Python using generators to reverse the string.
+Input String = “Consultadd Training”
+"""
+
+
+
+if __name__ == "__main__":
+    students = ['Smit', 'Jaya', 'Rayyan']
+    subjects = ['CSE', 'Networking', 'Operating System']
+    makeDict(students,subjects)
