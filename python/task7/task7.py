896f0cb118e0959c523deb698770c8a58bc138ae
siddhantwho <71570870+siddhantwho@users.noreply.github.com>
2021-02-24 20:08:58 -0500

    added q1 to task7

---
+++ python/task7/task7.py
@@ -0,0 +1,36 @@
+"""
+1. Write a program that calculates and prints the value according to the given formula:
+Q= Square root of [(2*C*D)/H]
+Following are the fixed values of C and H:
+C is 50.
+H is 30.
+D is a variable whose values should be input to your program in a comma-separated sequence.
+"""
+def Q(*D):
+    C = 50
+    H = 30
+    for i in D:
+        print ((2*C*i/H))
+
+"""
+2. Define a class named Shape and its subclass Square. The Square class has an init function which
+takes length as argument. Both classes have an area function which can print the area of the shape
+where Shape’s area is 0 by default.
+"""
+class Shape:
+    def area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+    def area(self):
+        return self.length**2
+
+
+
+
+
+
+if __name__ == "__main__":
+    Q(1,2,3)
