0d1e4a2f7b855ed7635abb74b59732e077fb47ba
siddhantwho <71570870+siddhantwho@users.noreply.github.com>
2021-03-11 10:54:55 -0500

    added views to firstapplication

--- django/project-1/firstproject/firstapplication/views.py
+++ django/project-1/firstproject/firstapplication/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def hello(request):
     return HttpResponse("Hello world!")
 
-def index(request):
+def bye(request):
     return HttpResponse("Bye world!")
-
