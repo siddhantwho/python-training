73f53fd52063ab0044ad476e8181b824c6fec26f
siddhantwho <71570870+siddhantwho@users.noreply.github.com>
2021-03-12 14:15:07 -0500

    added person model to fisrstapp

--- django/project-1/firstproject/firstapplication/admin.py
+++ django/project-1/firstproject/firstapplication/admin.py
@@ -1,3 +1,6 @@
+from .models import Person
 from django.contrib import admin
 
+
 # Register your models here.
+admin.site.register(Person)
--- django/project-1/firstproject/firstapplication/models.py
+++ django/project-1/firstproject/firstapplication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=10)
+    id_number = models.IntegerField()
+    contact = models.TextField()
+    address= models.TextField()
+
+    def __str__(self):
+        return self.name
