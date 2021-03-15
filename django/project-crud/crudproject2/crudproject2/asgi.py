ab5ff37f134f521c782e5eeced50593f38faad29
siddhantwho <71570870+siddhantwho@users.noreply.github.com>
2021-03-15 19:35:41 -0400

    modified crud project structure

--- django/project-crud/crudproject2/crudproject2/asgi.py
+++ django/project-crud/crudproject2/crudproject2/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for crudproj project.
+ASGI config for crudproject2 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudproj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudproject2.settings')
 
 application = get_asgi_application()
--- django/project-crud/crudproject2/crudproject2/wsgi.py
+++ django/project-crud/crudproject2/crudproject2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for crudproj project.
+WSGI config for crudproject2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudproj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudproject2.settings')
 
 application = get_wsgi_application()
