d52f765a66f4bf00eadb1e20318cb8abb0022574
siddhantwho <71570870+siddhantwho@users.noreply.github.com>
2021-03-18 02:24:30 -0400

    completed django crud assignment

--- django/crud-assign/blogproj/blogapp/urls.py
+++ django/crud-assign/blogproj/blogapp/urls.py
@@ -3,5 +3,8 @@ from blogapp import views
 
 urlpatterns = [
     path('', views.blog_list, name='blog_list'),
-    path('view/<int:pk>', views.blog_view, name='blog_post')
+    path('view/<int:pk>', views.blog_view, name='blog_post'),
+    path('create', views.blog_create, name='blog_create'),
+    path('edit/<int:pk>', views.blog_update, name='blog_edit'),
+    path('delete/<int:pk>', views.blog_delete, name='blog_delete')
 ]
--- django/crud-assign/blogproj/blogapp/views.py
+++ django/crud-assign/blogproj/blogapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.forms import ModelForm
 from .models import Blog
+from django import forms
 
 # Create your views here.
 
@@ -20,7 +21,25 @@ def blog_view(request, pk, template_name='blogpost.html'):
     blog = get_object_or_404(Blog, pk=pk)
     return render(request, template_name, {'object': blog})
 
+def blog_create(request, template_name='blogform.html'):
+    form = BlogForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('blog_list')
+    return render(request, template_name, {'form': form})
 
+def blog_update(request, pk, template_name='blogform.html'):
+    blog= get_object_or_404(Blog, pk=pk)
+    form = BlogForm(request.POST or None, instance=blog)
+    if form.is_valid():
+        form.save()
+        return redirect('blog_list')
+    return render(request, template_name, {'form':form})
 
-
+def blog_delete(request, pk, template_name='blogdelete.html'):
+    blog = get_object_or_404(Blog, pk=pk)
+    if request.method=='POST':
+        blog.delete()
+        return redirect('blog_list')
+    return render(request, template_name, {'object':blog})
  
